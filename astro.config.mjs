014229002e8e6eb5f7bb595b986ef46d0386afd4
Paul Klink <paul@klink.au>
2024-12-02 11:54:44 +1100

    Start improve sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-    title: 'hi'
+    title: 'hi',
+    sidebar: [
+      {slug: 'architecture'},
+      {slug: 'getting-started/installation'},
+      {label: 'Xilytix', link: 'https://xilytix.com'},
+    ]
   })]
 });
