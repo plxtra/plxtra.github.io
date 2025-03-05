70e68346c8e80d8336fc488cfe2930063f95a79b
Paul Klink <paul@klink.au>
2025-03-05 13:49:25 +1100

    Upgrade Astro to 5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { defineCollection } from 'astro:content';
 
 export const collections = {
-    docs: defineCollection({ schema: docsSchema() }),
+    docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
